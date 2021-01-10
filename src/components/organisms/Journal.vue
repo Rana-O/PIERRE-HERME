1faa199bc7081c8c92c3561ee98288de62fe99cf
Rana-O <r.000918000@gmail.com>
2021-01-10 18:35:25 +0900

    Journal.vueにタイトル追加、CSS調整

--- src/components/organisms/Journal.vue
+++ src/components/organisms/Journal.vue
@@ -1,5 +1,10 @@
 <template>
-  <div id="journal">ジャーナル
+  <div id="journal">
+    <div class="journal-title">
+      <a>The Journal
+        <span>news and topics</span>
+      </a>
+    </div>
     <JournalItems />
   </div>
 </template>
@@ -16,5 +21,20 @@ export default {
 </script>
 
 <style scoped>
-
+  #journal {
+    width: 95%;
+    margin: 0 auto;
+    position: relative;
+    background-color: white;
+  }
+  .journal-title {
+    font-size: 30px;
+    line-height: 1em;
+    font-family: "adobe-caslon-pro","Yu Mincho",YuMincho,"HG明朝E","ＭＳ Ｐ明朝","ＭＳ 明朝",seri
+  }
+  .journal-title span {
+    display: block;
+    font-size: 16px;
+    letter-spacing: .5px;
+  }
 </style>
