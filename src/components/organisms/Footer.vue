6d72a2cf14aa6fe310bf33652d1147fc374a7c15
Rana-O <r.000918000@gmail.com>
2021-01-10 22:21:29 +0900

    サイトマップ追加、css調整

--- src/components/organisms/Footer.vue
+++ src/components/organisms/Footer.vue
@@ -1,22 +1,26 @@
 <template>
-  <div id="footer">フッター
+  <div id="footer">
     <FooterGuide />
+    <SiteMap />
   </div>
 </template>
 
 <script>
 import FooterGuide from '@/components/organisms/FooterGuide.vue'
+import SiteMap from '@/components/organisms/SiteMap.vue'
 
 export default {
   name: 'Footer',
   components: {
-    FooterGuide
+    FooterGuide,
+    SiteMap
   }
 }
 </script>
 
 <style scoped>
   #footer {
-    background-color: black;
+    background-color:#646466;
+    color: white;
   }
 </style>
---
+++ src/components/organisms/SiteMap.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="site-map">サイトマップ</div>
+</template>
+
+<script>
+export default {
+  name: 'SiteMap'
+}
+</script>
+
+<style scoped>
+
+</style>
