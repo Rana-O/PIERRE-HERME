18e7b0aa8bf125f0fd157508b657abab7635a855
Rana-O <r.000918000@gmail.com>
2021-01-10 22:06:54 +0900

    フッターガイド作った

---
+++ src/components/organisms/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="footer">フッター
+    <FooterGuide />
+  </div>
+</template>
+
+<script>
+import FooterGuide from '@/components/organisms/FooterGuide.vue'
+
+export default {
+  name: 'Footer',
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style scoped>
+  #footer {
+    background-color: black;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 import { faShoppingBag } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faQuestionCircle)
 library.add(faShoppingBag)
+library.add(faArrowUp)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
