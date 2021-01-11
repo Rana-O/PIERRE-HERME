08e32c7b80bb1fbe549756f404da137343e66762
Rana-O <r.000918000@gmail.com>
2021-01-11 20:59:21 +0900

    FEATUREの中身追加

--- src/components/organisms/Feature.vue
+++ src/components/organisms/Feature.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="feature">
     <SiteMapTitle title="FEATURE" />
+    <div class="site-map-columns">
+      <a href="#">
+        <img src="@/assets/footer/futter_bnr_phk.jpg">
+      </a>
+      <a href="#">
+        <img src="@/assets/footer/footer_fetish-fraise-pistache.jpg">
+      </a>
+    </div>
   </div>
 </template>
 
@@ -18,5 +26,12 @@ export default {
   #feature {
     width: calc((100%/3)*2);
     margin: 20px 0;
+  }
+  .site-map-columns {
+    display: flex;
+    justify-content: space-around;
+  }
+  .site-map-columns img {
+    width: 260px;
   }
 </style>
