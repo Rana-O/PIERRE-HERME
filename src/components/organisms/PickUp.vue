f235fce502821dd9522910a32fdecc3472b6487c
Rana-O <r.000918000@gmail.com>
2021-01-12 17:11:10 +0900

    App.vueの見た目調整

--- src/components/organisms/PickUp.vue
+++ src/components/organisms/PickUp.vue
@@ -1,30 +1,32 @@
 <template>
   <div id="pick-up">
     <Title title="PICK UP" />
-    <div class="pick-up-item-container">
-      <PickUpItem :image="pickUpFetish" imageTitle="FETISH Fraise Pistache" pickUpText="ピスタチオが持つかりかり感やほんのりと香ばしい風味と、イチゴのフレッシュな爽快感やフルーティな酸味が、高貴な味覚のコンビネーションを生んでいます。" />
-    </div>
-    <div class="pick-up-item-container">
-      <PickUpItem :image="pickUpMacaron" imageTitle="MACARONS" pickUpText="この時期だけのマカロン詰合わせを販売中" />
-    </div>
-    <div class="pick-up-item-container">
-      <PickUpItem :image="pickUpChocolat" imageTitle="CHOCOLATS" pickUpText="口の中でソフトにとろける上質なショコラ。多彩なラインナップはこちらから" />
-    </div>
-    <div class="pick-up-item-container">
-      <PickUpItem :image="pickUpGift" imageTitle="詰合せギフトBOX" pickUpText="大切な方へのギフトにもおすすめ、ギフトBOXのご案内" />
+    <div class="pick-up-container">
+      <div class="pick-up-item-container">
+        <PickUpItem :image="pickUpFetish" imageTitle="FETISH Fraise Pistache" pickUpText="ピスタチオが持つかりかり感やほんのりと香ばしい風味と、イチゴのフレッシュな爽快感やフルーティな酸味が、高貴な味覚のコンビネーションを生んでいます。" />
+      </div>
+      <div class="pick-up-item-container">
+        <PickUpItem :image="pickUpMacaron" imageTitle="MACARONS" pickUpText="この時期だけのマカロン詰合わせを販売中" />
+      </div>
+      <div class="pick-up-item-container">
+        <PickUpItem :image="pickUpChocolat" imageTitle="CHOCOLATS" pickUpText="口の中でソフトにとろける上質なショコラ。多彩なラインナップはこちらから" />
+      </div>
+      <div class="pick-up-item-container">
+        <PickUpItem :image="pickUpGift" imageTitle="詰合せギフトBOX" pickUpText="大切な方へのギフトにもおすすめ、ギフトBOXのご案内" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-// import Title from '@/components/atoms/Title.vue'
+import Title from '@/components/atoms/Title.vue'
 import PickUpItem from '@/components/molecs/PickUpItem.vue'
 import { pickUp } from '@/components/atoms/images'
 
 export default {
   name: 'PickUp',
   components: {
-    // Title,
+    Title,
     PickUpItem
   },
   computed: {
@@ -42,6 +44,8 @@ export default {
     background-color: white;
     width: 95%;
     margin: 0px auto;
+  }
+  .pick-up-container {
     display: flex;
     flex-wrap: wrap;
   }
