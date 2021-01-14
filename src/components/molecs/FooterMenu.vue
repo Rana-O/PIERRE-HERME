6eea20b57a7126a038f37500fc42bc8f770eaed4
Rana-O <r.000918000@gmail.com>
2021-01-14 13:53:32 +0900

    必要ファイル作成

---
+++ src/components/molecs/FooterMenu.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="footer-menu">フッタメニュー</div>
+</template>
+
+<script>
+export default {
+  name: 'FooterMenu'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/organisms/TestFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="test-footer">テスト
+    <FooterMenu />
+  </div>
+</template>
+
+<script>
+import FooterMenu from '@/components/molecs/FooterMenu.vue'
+
+export default {
+  name: 'TestFooter',
+  components: {
+    FooterMenu
+  }
+}
+</script>
+
+<style scoped>
+  #test-footer {
+    background-color: lightgrey;
+    color: white;
+    font-size: 14px;
+  }
+</style>
