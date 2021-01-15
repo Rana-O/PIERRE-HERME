89420e0e6e10758a64f621c9fc753494c9d18482
Rana-O <r.000918000@gmail.com>
2021-01-15 11:22:26 +0900

    みため調整

--- src/components/organisms/Carousel.vue
+++ src/components/organisms/Carousel.vue
@@ -7,12 +7,13 @@
       >
         <div class="carousel-image">
           <img src="@/assets/hero/mac_autumn-winter-2018_pc.jpg">
+          <p>マカロン詰合せ</p>
+          <p>ピエール・エルメ・パリ人気のマカロンを詰合わせたギフトを販売中</p>
         </div>
         <div class="carousel-image">
-          <img src="@/assets/hero/top_fraise-pistache-2020_pc.jpg">
-        </div>
-        <div class="carousel-image">
-          <img src="@/assets/hero/top_winter-gift-2020_pc.jpg">
+          <img src="@/assets/hero/private-delivery-1_pc.jpg">
+          <p>プライベートデリバリー</p>
+          <p>喜びあふれる味覚をお届けするプライベートデリバリーがはじまります</p>
         </div>
       </VueSlickCarousel>
   </div>
@@ -33,5 +34,32 @@ export default {
 </script>
 
 <style scoped>
+</style>
 
+<style>
+#carousel {
+  margin: 0 auto;
+  width: 900px;
+}
+#carousel button {
+  color: grey;
+  width: 15px;
+  height: 15px;
+}
+#carousel .slick-prev:before {
+  content: url("/img/arrow_prev.svg");
+}
+#carousel .slick-next:before {
+  content: url("/img/arrow_next.svg");
+}
+#carousel .slick-dots button:before {
+  content: '▪';
+  font-size: 25px;
+}
+#carousel .slick-prev {
+  left: -35px;
+}
+#carousel .slick-next {
+  right: -35px;
+}
 </style>
