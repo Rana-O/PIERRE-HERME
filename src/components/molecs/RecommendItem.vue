204fd1cab97972d8fd668be322be3d8d5af503d8
Rana-O <r.000918000@gmail.com>
2021-01-17 14:22:52 +0900

    RecommendItemをtypescriptにした

--- src/components/molecs/RecommendItem.vue
+++ src/components/molecs/RecommendItem.vue
@@ -5,22 +5,25 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Vue, { PropType } from 'vue'
 import RecommendImage from '@/components/atoms/RecommendImage.vue'
 import RecommendText from '@/components/atoms/RecommendText.vue'
 
-export default {
+export default Vue.extend({
   name: 'RecommendItem',
+
   components: {
     RecommendImage,
     RecommendText
   },
-  props: [
-    'recommendImage',
-    'name',
-    'price'
-  ]
-}
+
+  props: {
+    recommendImage: String as PropType<string>,
+    name: String as PropType<string>,
+    price: String as PropType<string>
+  }
+})
 </script>
 
 <style scoped>
